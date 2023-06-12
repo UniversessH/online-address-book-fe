59aadba7e312dca3f8b2f2948c27e34bc8b22590
universess <universess@qq.com>
2023-06-12 21:27:55 +0800

    feat: 完成禁用账号与启用账号

--- src/pages/MainPage/AdminMainPage/components/AuthorityManage/index.tsx
+++ src/pages/MainPage/AdminMainPage/components/AuthorityManage/index.tsx
@@ -1,20 +1,47 @@
-import React from "react";
+import React, { useRef } from "react";
 import { ProForm, ProFormText } from "@ant-design/pro-components";
-import { Button, Space } from "antd";
+import type { ProFormInstance } from "@ant-design/pro-components";
+import { Button, Space, message } from "antd";
+import { manageApprovedAccount } from "../../../../../network/apis";
 
 const AuthorityManage: React.FC = () => {
+  const formRef = useRef<ProFormInstance>();
+
+  const clickManageBtnHandler = async (isDisable: boolean) => {
+    try {
+      await manageApprovedAccount(isDisable);
+      message.success(`${isDisable ? "已将该账号禁用" : "已启用该账号"}`);
+      formRef?.current?.setFieldsValue({
+        student_id: "",
+      });
+    } catch (error) {
+      console.log(error);
+      message.error("操作出错");
+    }
+  };
+
   return (
     <>
       <div style={{ fontSize: "20px", marginBottom: "16px", color: "#4780f7" }}>
         账户权限管理
       </div>
       <ProForm
+        formRef={formRef}
         submitter={{
           render: () => {
             return [
               <Space>
-                <Button type="primary">启用</Button>
-                <Button type="primary" danger>
+                <Button
+                  type="primary"
+                  onClick={() => clickManageBtnHandler(false)}
+                >
+                  启用
+                </Button>
+                <Button
+                  type="primary"
+                  onClick={() => clickManageBtnHandler(true)}
+                  danger
+                >
                   封禁
                 </Button>
               </Space>,
